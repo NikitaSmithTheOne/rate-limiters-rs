128a20d3812ddfb3c536bd021247e8d3f49846bf
NikitaSmithTheOne <n.galkin.official@gmail.com>
2025-08-23 15:11:24 +0500

    docs: +sliding window log usage examples

---
+++ examples/sliding_window_log_shared_usage.rs
@@ -0,0 +1,32 @@
+// cargo run --example token_bucket_shared_usage
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+
+use rate_limiters::sliding_window_log::SlidingWindowLogShared;
+use rate_limiters::token_bucket::r#impl::RateLimiterShared;
+
+fn main() {
+    let bucket = Arc::new(SlidingWindowLogShared::new(2, 1));
+
+    let mut handles = vec![];
+    for i in 0..100 {
+        let bucket_clone = Arc::clone(&bucket);
+        handles.push(thread::spawn(move || {
+            let is_acquired = bucket_clone.try_acquire(1);
+            println!(
+                "Thread #{i} → {}",
+                if is_acquired {
+                    "Allowed"
+                } else {
+                    "Rate limited"
+                }
+            );
+        }));
+
+        thread::sleep(Duration::from_millis(250));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
